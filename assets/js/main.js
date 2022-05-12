60a209e82c225cb053325c827e89c99e7efbc497
Hridoy <iamhridoyhaque.com>
2022-05-12 10:41:42 +0600

    complete project

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,4 +17,21 @@ $(document).ready(function () {
     nextArrow: '<button type="button" class="slick-next"><i class="fas fa-long-arrow-alt-right"></i></button>',
     dots: true,
   });
+
+ const header = document.querySelector('.header-area')
+
+ function activeHeader(){
+   const scrolltop = window.scrollY
+   if(scrolltop > 100){
+     header.classList.add('header-fixed')
+   }else{
+     header.classList.remove('header-fixed')
+   }
+ }
+
+ window.addEventListener('scroll',activeHeader)
+ window.addEventListener('load',activeHeader)
+
 });
+
+
